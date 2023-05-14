0a2410fef0d22d1b3bdd2d57d204af8095577852
ThereIsGrace <universe2879@gmail.com>
2023-05-15 00:11:26 +0900

    행 삭제하기

---
+++ src/oracle/sec05/BoardDeleteExample.java
@@ -0,0 +1,48 @@
+package oracle.sec05;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class BoardDeleteExample {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		try {
+			//JDBC Driver 등록
+			Class.forName("oracle.jdbc.OracleDriver");
+			
+			//연결하기
+			conn = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521/xe",
+					"jaeeun",
+					"12345");
+			
+			//매개변수화된 SQL 문 작성
+			String sql = "DELETE FROM boards WHERE bwriter=?";
+			
+			//PreparedStatement 얻기 및 값 지정
+			PreparedStatement psmt = conn.prepareStatement(sql);
+			psmt.setString(1, "winter");
+			
+			//SQL 문 실행
+			int rows = psmt.executeUpdate();
+			System.out.println("삭제된 행 수: " + rows);
+			
+			//PreparedStatement 닫기
+			psmt.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(conn!=null) {
+				try {
+					//연결 끊기
+					conn.close();
+				}catch(SQLException e) {
+					
+				}
+			}
+		}
+	}
+}
