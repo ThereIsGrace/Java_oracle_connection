194d94d22704bd50c25307a9383273e2decddd41
ThereIsGrace <universe2879@gmail.com>
2023-05-15 00:11:43 +0900

    User Dto

---
+++ src/oracle/sec06/User.java
@@ -0,0 +1,12 @@
+package oracle.sec06;
+
+import lombok.*;
+
+@Data
+public class User {
+	private String userId;
+	private String userName;
+	private String userPassword;
+	private int userAge;
+	private String userEmail;
+}
